cd6f8153b49b8f8093068ab190d97410fa8ed60e
Sterling Davenport <sterling.w.davenport@gmail.com>
2019-04-27 16:58:01 -0400

    added script file

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+//member list
+var members = ['Dave Grohl', 'Nate Mendel', 'Pat Smear', 'Taylor Hawkins', 'Rami Jaffee', 'Chris Shiflett'];
+//tour dates object
+var tourDates = {
+  Chicago: "July 29th and July 30th, 2018",
+  Seattle: "September 1st, 2018",
+  Edmonton: "September 4th, 2018"
+};
+//while loop
+var i = 0;
+
+while (i < 5) {
+  ('Times running through the loop x' + i);
+
+  i++;
+}
